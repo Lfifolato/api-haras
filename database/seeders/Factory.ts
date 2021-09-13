097e661fb9daf7d841f38d57dcee9a416d02cf18
lfifolato <ldalri1999@gmail.com>
2021-09-12 23:50:56 -0300

    ok

--- database/seeders/Factory.ts
+++ database/seeders/Factory.ts
@@ -1,8 +1,8 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { UserFactory } from 'Database/factories'
+// import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+// import { UserFactory } from 'Database/factories'
 
-export default class FactorySeeder extends BaseSeeder {
-  public async run() {
-    const user = await UserFactory.createMany(15000)
-  }
-}
+// export default class FactorySeeder extends BaseSeeder {
+//   public async run() {
+//     const user = await UserFactory.createMany(15000)
+//   }
+// }
